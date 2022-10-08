79aa63f96bbacbe7e4d51cda7139e9755cb1ec1e
Haseeb Ahmed <kkmj989@astrazeneca.net>
2022-10-08 22:32:07 +0100

    Updating testing F

--- inflammation/models.py
+++ inflammation/models.py
@@ -56,11 +56,15 @@ def patient_normalise(data):
 
     Negative values are rounded to 0.
     """
+    if not isinstance(data, np.ndarray):
+        raise TypeError('data input should be ndarray')
+    if len(data.shape) != 2:
+        raise ValueError('inflammation array should be 2-dimensional')
     if np.any(data < 0):
         raise ValueError('Inflammation values should not be negative')
-    max = np.nanmax(data, axis=1)
+    max_data = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
-        normalised = data / max[:, np.newaxis]
+        normalised = data / max_data[:, np.newaxis]
     normalised[np.isnan(normalised)] = 0
     normalised[normalised < 0] = 0
     return normalised
